ee101d25916a5426ca567435ac9b6df4fd6e4b44
anato1yrain <155212944+anato1yrain@users.noreply.github.com>
2024-01-31 18:15:02 +0800

    Update const.ts

--- src/const.ts
+++ src/const.ts
@@ -3,7 +3,7 @@ import { type DB_PROPERTIES_KEYS, NotionPropTypesEnum, ItemStatus } from './type
 export const ALL_STATUS = /^(?:最近)?(看过|听过|读过|玩过|在看|在听|在读|在玩|想看|想听|想读|想玩)/;
 
 export const RATING_TEXT = {
-  很差: 1,
+  很差: ☆,
   较差: 2,
   还行: 3,
   推荐: 4,
